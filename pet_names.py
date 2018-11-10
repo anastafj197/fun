78f4524af5718448e57ae085e3089ee3bcbc71b7
anastafj197 <frankie.anastasia@gmail.com>
2018-11-09 22:07:45 -0500

    Update pet_names.py

--- pet_names.py
+++ pet_names.py
@@ -2,8 +2,8 @@ import random
 
 # Program to generate randomized names when you are bored of calling your pet the same thing
 
-beg = ["sn","m", "bab", "po"]
-mid = ["oop", "a", "oosh", "oo", "ok"]
+beg = ["sn","m", "bab", "po", "sl"]
+mid = ["oop", "a", "oosh", "oo", "ok", "uth"]
 end = ["er","ie", "ki", "ku", "u"]
 
 name = random.choice(beg) + random.choice(mid) + random.choice(end)

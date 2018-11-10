54220228f762ffb2593d7e4d54dfd2cc77cfeb3e
anastafj197 <frankie.anastasia@gmail.com>
2018-11-09 21:28:44 -0500

    Added choices
    Some names I call my dog

--- pet_names.py
+++ pet_names.py
@@ -1,1 +1,11 @@
+import random 
+
 # Program to generate randomized names when you are bored of calling your pet the same thing
+
+beg = ["sn","m", "bab", "po"]
+mid = ["oop", "a", "oosh", "oo", "ok"]
+end = ["er","ie", "ki", "ku", "u"]
+
+name = random.choice(beg) + random.choice(mid) + random.choice(end)
+
+print(name)
